601f0d9f8e318294263cd5d7b157cfd1d2d4289c
root <piotr62@live.fr>
2022-08-30 04:53:00 -0700

    fixin' lady betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_t - inserts a fresh node as the left child
+ * binary_tree_insert_left - inserts a fresh node as the left child
  * of another node
  * @parent: pointer to the node to insert a left-child in
  * @value: value to store in new node
