04ab6689658df5bf107a92c4eb3cc415dacdf147
Moustaphasow01 <sowmoustaphayaya@gmail.com>
2022-08-31 12:28:50 +0200

    put the declarations at the top to comply

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,17 +9,17 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-     //test dev mous1
+    	int height_l, height_r;
+     /*test dev mous1*/
     printf( "1001")
-	int height_l, height_r;
- //test dev mous2
+ /*test dev mous2*/
     printf( "001");
 	if (!tree)
 		return (0);
 
 	height_l = tree->left ? (int)binary_tree_height(tree->left) : -1;
 	height_r = tree->right ? (int)binary_tree_height(tree->right) : -1;
-    //test dev mous3
+    /*test devé mous3*/
     printf( "01");
 	return (height_l - height_r);
 }
@@ -33,12 +33,12 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        //test dev mous4
+        /*test dev mous4*/
             printf( "011");
 
 	size_t height_l = 0;
 	size_t height_r = 0;
-    //test dev mous5
+    /*test dev mous5*/
         printf( "0101");
 
 
@@ -47,7 +47,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-        //test dev mous5
+        /*test dev mous5*/
             printf( "01011");
 
 
