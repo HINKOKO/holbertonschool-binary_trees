17badad33062e996722fcfb814d8c41411ba8f52
Moustaphasow01 <sowmoustaphayaya@gmail.com>
2022-08-31 12:34:19 +0200

    progam is compile lets fix exected bug

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,17 +10,12 @@
 int binary_tree_balance(const binary_tree_t *tree)
 {
     	int height_l, height_r;
-     /*test dev mous1*/
-    printf( "1001");
- /*test dev mous2*/
-    printf( "001");
+  
 	if (!tree)
 		return (0);
 
 	height_l = tree->left ? (int)binary_tree_height(tree->left) : -1;
 	height_r = tree->right ? (int)binary_tree_height(tree->right) : -1;
-    /*test devé mous3*/
-    printf( "01");
 	return (height_l - height_r);
 }
 
@@ -35,19 +30,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
     size_t height_l = 0;
 	size_t height_r = 0;
-        /*test dev mous4*/
-            printf( "011");
-
-    /*test dev mous5*/
-        printf( "0101");
 	if (!tree)
 		return (0);
 
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-        /*test dev mous5*/
-            printf( "01011");
-
-
 	return (height_l > height_r ? height_l : height_r);
 }
