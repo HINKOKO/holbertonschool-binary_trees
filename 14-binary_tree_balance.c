81e877794a95db9c1e4c626c5326e985eab1ac99
Moustaphasow01 <sowmoustaphayaya@gmail.com>
2022-08-31 12:20:09 +0200

    compilation test problem

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,14 +13,14 @@ int binary_tree_balance(const binary_tree_t *tree)
     printf( "1001")
 	int height_l, height_r;
  //test dev mous2
-    printf( "001")
+    printf( "001");
 	if (!tree)
 		return (0);
 
 	height_l = tree->left ? (int)binary_tree_height(tree->left) : -1;
 	height_r = tree->right ? (int)binary_tree_height(tree->right) : -1;
     //test dev mous3
-    printf( "01")
+    printf( "01");
 	return (height_l - height_r);
 }
 
@@ -34,12 +34,12 @@ int binary_tree_balance(const binary_tree_t *tree)
 size_t binary_tree_height(const binary_tree_t *tree)
 {
         //test dev mous4
-            printf( "011")
+            printf( "011");
 
 	size_t height_l = 0;
 	size_t height_r = 0;
     //test dev mous5
-        printf( "0101")
+        printf( "0101");
 
 
 	if (!tree)
@@ -48,7 +48,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
         //test dev mous5
-            printf( "01011")
+            printf( "01011");
 
 
 	return (height_l > height_r ? height_l : height_r);
