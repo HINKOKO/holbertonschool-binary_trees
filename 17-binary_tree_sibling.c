e1df48ceca7892419dfb225a6f208b305cf31b39
root <piotr62@live.fr>
2022-08-31 06:15:25 -0700

    Task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -3,10 +3,15 @@
 /**
  * binary_tree_sibling - finds the sibling of a node
  * @node: pointer to the node to find the sibling
+ * Return: Null if node/parent are null or no sibling found
+ * otherwise return the sibling node
+ */
 
- 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
-
+	if (node == node->parent->left)
+		return (node->parent->right);
+	return (node->parent->left);
+}
