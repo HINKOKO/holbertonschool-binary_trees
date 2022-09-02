01fdd264345f73c2a7f9738f19bb8d9934959c27
root <piotr62@live.fr>
2022-09-02 08:29:20 -0700

    array to bst test//Terminal okay

---
+++ 112-array_to_bst.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - builds a BST from an array
+ * @array: pointer to first element of array to be converted
+ * @size: number of element of array
+ * Return: returns a pointer to root node of created BST
+ * or NULL on failure
+ */
+
+bst_t *array_to_bst(int *array, size_t size)
+{
+	size_t i;
+	bst_t *root = NULL;
+
+	for (i = 0; i < size; i++)
+		bst_tree(&root, array[i]);
+	return (root);
+}
